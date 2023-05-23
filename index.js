c38a7afd496b7f1dc279c59e5363e03d6674f530
rgibbons131 <richiegibbons99@gmail.com>
2023-05-23 08:53:01 -0600

    added stuff

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+const dotenv = require("dotenv");
+const MongoClient = require("mongodb").MongoClient;
+const mongodb = require("./db/connect");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+app
+  .use(bodyParser.json())
+  .use(cors())
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+  })
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use("/", users);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
